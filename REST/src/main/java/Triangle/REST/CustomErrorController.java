66353799bc21e79742bd7f7d5afe2c4aa882e884
Burrouwill <wgr.burroughs@gmail.com>
2023-09-06 09:35:04 +1200

    Error hhandling seems to work fine for the most part. There is some slight missclassification of errors when client misstypes url i.e. they get a 500 when it should probably be a 400 (I think).

--- REST/src/main/java/Triangle/REST/CustomErrorController.java
+++ REST/src/main/java/Triangle/REST/CustomErrorController.java
@@ -1,2 +1,28 @@
-package Triangle.REST;public class CustomErrorController {
+package Triangle.REST;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@ControllerAdvice
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public ResponseEntity<String> handleError() {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Error: The requested page does not exist.");
+    }
+
+    public String getErrorPath() {
+        return "/error";
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Error: Please ensure url is correctly formatted and contains all three values / triangle sides.");
+    }
 }
--- REST/src/main/java/Triangle/REST/TriangleController.java
+++ REST/src/main/java/Triangle/REST/TriangleController.java
@@ -1,36 +1,34 @@
 package Triangle.REST;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TriangleController {
 
     @GetMapping("/triangle")
-    public String classifyTriangle(
+    public ResponseEntity<String> classifyTriangle(
             @RequestParam double a,
             @RequestParam double b,
             @RequestParam double c
     ) {
-        try {
-            if (a <= 0 || b <= 0 || c <= 0) {
-                return "Error: Side lengths must be positive values.";
-            }
+        if (a <= 0 || b <= 0 || c <= 0) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Error: Side lengths must be positive values.");
+        }
 
-            if (a + b <= c || a + c <= b || b + c <= a) {
-                return "Error: Invalid triangle. The sum of any two sides must be greater than the third side.";
-            }
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Error: Invalid triangle. The sum of any two sides must be greater than the third side.");
+        }
 
-            if (a == b && b == c) {
-                return "EQUILATERAL";
-            } else if (a == b || b == c || a == c) {
-                return "ISOCELES";
-            } else {
-                return "SCALENE";
-            }
-        } catch (Exception e) {
-            return "Error: " + e.getMessage();
+        if (a == b && b == c) {
+            return ResponseEntity.ok("EQUILATERAL");
+        } else if (a == b || b == c || a == c) {
+            return ResponseEntity.ok("ISOCELES");
+        } else {
+            return ResponseEntity.ok("SCALENE");
         }
     }
 }
